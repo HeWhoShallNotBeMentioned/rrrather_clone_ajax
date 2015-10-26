4c2d8d7a3a7c8726b1232e899a1b884dab2b8fd9
Chris <cunderwoodmn@gmail.com>
2015-10-26 12:17:11 -0700

    add data for the seeds database and put some formating into the Questions ul list.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Question.create(option1: "be a doctor?", option2: "be a lawyer?")
+Question.create(option1: "have a small dog?", option2: "have a big dog?")
+Question.create(option1: "have mermaids be real?", option2: "have unicorns be real?")
+Question.create(option1: "be a ninja?", option2: "be a wizard?")
