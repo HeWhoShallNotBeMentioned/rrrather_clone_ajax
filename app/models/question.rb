1ac9c84e4fa44f794591384bc28d21c607af47e4
Chris <cunderwoodmn@gmail.com>
2015-10-26 10:57:12 -0700

    add code for passing test of option 2in question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ActiveRecord::Base
   validates :option1, :presence => true
+  validates :option2, :presence => true
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Question do
   it { should validate_presence_of :option1}
+  it { should validate_presence_of :option2}
 end
